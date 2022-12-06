a8153032a45231e86f6e5b1c08949c41a2ebc044
Daniel Weigel <daniel@weigel.host>
2022-12-06 06:08:40 +0100

    WiP

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,6 +1,6 @@
 # type: ignore 
-import timeit
 import re
+import timeit
 
 start_time = timeit.default_timer()
 
@@ -18,8 +18,7 @@ yyyy = today.strftime("%Y")
 #os.system('aocd ' + dd + ' ' + yyyy + '  > ./Advent-Of-Code/' + yyyy + '/data/day' + dd + '.txt')
 
 #file = './Advent-Of-Code/' + yyyy + '/data/day' + dd + '.txt'
-#file = 'C:\\Users\\daniel.weigel\\Documents\\Visual Studio 2022\\GIT\\Advent-Of-Code\\2022\\data\\day05.txt'
-file = 'C:\\Users\\daniel.weigel\\Documents\\Visual Studio 2022\\GIT\\Advent-Of-Code\\2022\\data\\example.txt'
+file = './Advent-Of-Code/' + yyyy + '/data/example.txt'
 
 newlist=[]
 one = []
@@ -32,19 +31,36 @@ seven = []
 eight = []
 nine = []
 containers = 0
+boxes = []
+boxes2 = []
+boxes_rotate =  []
+
+def rotate(array):
+    R, C = len(array), len(array[0])
+    newArr = [[None] * R for _ in range(C)]
+    for c in range(C):
+        for r in range(R-1, -1, -1):
+            newArr[C-c-1][r] = array[r][c]
+    return newArr    
 
 with open(file, 'r',  newline='\n') as read:
     data = read.readlines()#.split('\n')
     for lines in data:
-        newlist.append(lines.replace('    ', ' [_] ').strip())
+        newlist.append(lines.replace('    ', '_').replace('[', '').replace(']','').replace(' ', '').strip())
     for i in range(len(newlist)):
-        print(newlist[i])
+        if newlist[i] == '':
+            break
+        elif newlist[i] != '':
+            containers += 1
+    boxes = newlist[:(containers-1)]
+    boxes_rotate = rotate(list(boxes))
+    for i in range(len(boxes_rotate)):
+        for j in range(len(boxes_rotate[0])):
+            if boxes_rotate[i][j] == '_':
+                boxes_rotate[i].pop(0)
+        print(boxes_rotate)
+                
     
-
-
-
-    print(newlist[:(containers-1)])
-    print(newlist[(containers+1):])
     
 
 
