0e728b0acce117d9b45316eabe2bef262775ee7b
daniel.weigel <daniel.weigel@hetzner.com>
2023-12-25 07:32:54 +0100

    still some problems find the correct pattern in Part 2

--- 2023/Day1.sh
+++ 2023/Day1.sh
@@ -10,15 +10,18 @@ START=$(date +%s.%N)
 while read -r LINE
 do
     DIGITS=$(echo "$LINE" | grep -o -P '[0-9]' | awk 'BEGIN{FS="";OFS=""} {if (NR==1) f=$0; l=$0} END{print f,l}')
-#    NORMAL=$(echo "$LINE" | grep -o -m 1 -P '[0-9]|one|two|three|four|five|six|seven|eight|nine')
-#    REVERSED=$(echo "$LINE" | rev |grep -o -m 1 -P '[0-9]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin')
-#    NORMALCONVERT=$(echo "$NORMAL" | sed 's/one/1/g; s/two/2/g; s/three/3/g; s/four/4/g; s/five/5/g; s/six/6/g; s/seven/7/g; s/eight/8/g; s/nine/9/g')
-#    REVERSEDCONVERT=$(echo "$REVERSED" | sed 's/eno/1/g; s/owt/2/g; s/eerht/3/g; s/ruof/4/g; s/evif/5/g; s/xis/6/g; s/neves/7/g; s/thgie/8/g; s/enin/9/g' | rev)
-#    WORDS="${NORMALCONVERT}${REVERSEDCONVERT}"
-#    echo "$NORMALCONVERT $REVERSEDCONVERT"
     ((SUMP1+=DIGITS))
-#    ((SUMP2+=WORDS))
 done < "$INPUT"
+
+while read -r LINE
+do
+    FIRST=$(echo "$LINE" | grep -o -m 1 -P '[0-9]|one|two|three|four|five|six|seven|eight|nine' | head -n 1)
+    LAST=$(echo "$LINE" | sed 's/oneight/oneeight/g' | grep -o -P '[0-9]|nine|eight|seven|six|five|four|three|two|one' | tac | head -n 1)
+    FIRSTCONVERT=$(echo "$FIRST" | sed 's/one/1/g; s/two/2/g; s/three/3/g; s/four/4/g; s/five/5/g; s/six/6/g; s/seven/7/g; s/eight/8/g; s/nine/9/g')
+    LASTCONVERT=$(echo "$LAST" | sed 's/one/1/g; s/two/2/g; s/three/3/g; s/four/4/g; s/five/5/g; s/six/6/g; s/seven/7/g; s/eight/8/g; s/nine/9/g')
+    WORDS="${FIRSTCONVERT}${LASTCONVERT}"
+    ((SUMP2+=WORDS))
+done  < "$INPUT"
 
 # End timestamp
 END=$(date +%s.%N)
@@ -30,5 +33,5 @@ SEC=$(echo "$DIFF % 60" | bc)
 
 # Print the sum and runtime
 echo "Part 1: $SUMP1"
-#echo "Part 2: $SUMP2"
+echo "Part 2: $SUMP2"
 echo "Runtime: $MIN Minutes $SEC Seconds"
